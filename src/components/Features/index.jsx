9a7081a331b2cca6855d75f2f1f97cac257afe96
wellison E Brito <wellysone.brito@gmail.com>
2022-01-08 16:08:41 -0300

    Screen Pages Finished

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -4,15 +4,19 @@ import "./styles.scss";
 
 export const Features = () => {
     return (
-        <div className='main' id='main'>
-            <div className='main-text'>
-                <h1>
-                    Rachi,
-                    <br /> é tudo que você precisa em um só lugar.
-                </h1>
-                <button>Cadastrar-se</button>
+        <div className='container' id='main'>
+            <div className='background-container'>
+                <div className='main'>
+                    <div className='main-text'>
+                        <h1>
+                            Rachi,
+                            <br /> é tudo que você precisa em um só lugar.
+                        </h1>
+                        <button>Cadastrar-se</button>
+                    </div>
+                    <img src={ManWithNotebookImg} alt='man-with-notebook' />
+                </div>
             </div>
-            <img src={ManWithNotebookImg} alt='man-with-notebook' />
         </div>
     );
 };

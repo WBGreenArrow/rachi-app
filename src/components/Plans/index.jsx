802f60f7f6f5fca56624f0fd74269a78fe4b4dc6
wellison E Brito <wellysone.brito@gmail.com>
2022-01-08 01:09:42 -0300

    Created Cardes Plans

--- src/components/Plans/index.jsx
+++ src/components/Plans/index.jsx
@@ -11,7 +11,12 @@ export const Plans = () => {
             <div className='container-carders'>
                 <div className='carder'>
                     <h2>Bronze</h2>
-                    <h3>R$28/mês</h3>
+                    <div className='price'>
+                        <h3>R$</h3>
+                        <h3>28</h3>
+                        <h3>/mês</h3>
+                    </div>
+
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.
                     </p>
@@ -19,8 +24,12 @@ export const Plans = () => {
                 </div>
                 <div className='carder'>
                     <h2>Prata</h2>
-                    <div>preferido</div>
-                    <h3>R$57/mês</h3>
+                    <label>preferido</label>
+                    <div className='price'>
+                        <h3>R$</h3>
+                        <h3>57</h3>
+                        <h3>/mês</h3>
+                    </div>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.
                     </p>
@@ -28,7 +37,11 @@ export const Plans = () => {
                 </div>
                 <div className='carder'>
                     <h2>Ouro</h2>
-                    <h3>R$94/mês</h3>
+                    <div className='price'>
+                        <h3>R$</h3>
+                        <h3>94</h3>
+                        <h3>/mês</h3>
+                    </div>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices.
                     </p>
